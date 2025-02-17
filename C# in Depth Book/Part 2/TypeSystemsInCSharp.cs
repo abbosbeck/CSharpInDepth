b7d50cc93d4b4a17e9e3689fcadc4f4edbfeb226
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-17 12:09:18 +0500

    PART2: Uderstand the difference between Tuple and Anonymous Types

---
+++ C# in Depth Book/Part 2/TypeSystemsInCSharp.cs
@@ -0,0 +1,22 @@
+﻿namespace Part2
+{
+    public class AnonymousTypesAndAnonymousTypesAndTuples
+    {
+        public void TupleTypes()
+        {
+            var book = (title: "C# in Depth", author: "Jon Skeet", age: 15);
+
+            book.title= "Hello world";
+
+            Console.WriteLine(book.title);
+        }
+
+        public void AnonymousType()
+        {
+            var book = new { Title = "C# in Depth", Author = "Jon Skeet", Age = 15};
+
+
+            Console.WriteLine(book.Title);
+        }
+    }
+}
--- C# in Depth Book/Program.cs
+++ C# in Depth Book/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Part2;
+
+var AnonymousTypesAndTuples = new AnonymousTypesAndAnonymousTypesAndTuples();
+
+AnonymousTypesAndTuples.TupleTypes();
+
+AnonymousTypesAndTuples.AnonymousType();
