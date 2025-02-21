55fbbc53e6b67bb1b8c085778497d83690f65921
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-21 20:36:28 +0500

    Implement Partial Method

---
+++ C# in Depth Book/Part 2/PartialClasses.cs
@@ -0,0 +1,21 @@
+﻿namespace Part2
+{
+    class PartialClasses
+    {
+
+    }
+
+    public partial class MyClass
+    {
+        public partial int ComputeValue();  // No implementation yet
+    }
+
+    public partial class MyClass
+    {
+        public partial int ComputeValue()  // Implemented here
+        {
+            return 42;
+        }
+    }
+
+}
