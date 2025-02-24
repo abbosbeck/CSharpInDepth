9857fd10bc5b2c854a803577dfb190de3031d618
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-24 22:31:49 +0500

    Type Safity & Language Revolution

---
+++ C# in Depth Book/Part 3/TypeInference.cs
@@ -0,0 +1,11 @@
+﻿namespace C__in_Depth_Book.Part_3
+{
+    public class TypeInference
+    {
+        public void Run()
+        {
+            var arrayExample = new[] { "a", "b"};
+        }
+    }
+}
+
