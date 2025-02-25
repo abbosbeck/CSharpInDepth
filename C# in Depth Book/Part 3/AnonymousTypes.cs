b497044bd8214d7524bd70f2eda0ce5fdc367b8e
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-25 14:25:09 +0500

    anonymous object creation expression.

---
+++ C# in Depth Book/Part 3/AnonymousTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace C__in_Depth_Book.Part_3
+{
+    public class AnonymousTypes
+    {
+        public void CreateAnAnonanonymousPbjectCreationExpression()
+        {
+            var pupil = new
+            {
+                Ism = "Abbos",
+                Familiya = "Abduqayumov",
+                Yosh = 21
+            };
+        }
+    }
+}
