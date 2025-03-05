98665cef7bd1f786d3202bfb32bf0eb60c7adb1d
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-05 09:23:52 +0500

    OOP: Getter And Setter

---
+++ C# in Depth Book/Part 2/OOP/Car.cs
@@ -0,0 +1,26 @@
+﻿namespace Part2.OOP
+{
+    class Car
+    {
+        public Car()
+        {
+            
+        }
+
+        public string MyProperty 
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                if(value == "new")
+                    text = "This value cannot be new";
+                text = value;
+            } 
+        }
+
+        private string text;
+    }
+}
