26112f577aa26996eac8608d90f87a9807b7a9a8
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-05 10:48:09 +0500

    How Base Class Works

---
+++ C# in Depth Book/Part 2/OOP/Animal.cs
@@ -0,0 +1,18 @@
+﻿namespace Part2.OOP
+{
+    public class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("Animal speaks...");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+}
