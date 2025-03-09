5e13d9575f1bee07a8f169f1191558426818b9fa
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-03-09 20:54:19 +0500

    Intehrentace

---
+++ C# in Depth Book/Part 2/OOP/ITwo.cs
@@ -0,0 +1,10 @@
+﻿namespace Part2.OOP
+{
+    public interface ITwo
+    {
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine("This is ITwo Interface. Result: ", a * b);
+        }
+    }
+}
---
+++ C# in Depth Book/Part 2/OOP/InterfaceCaller.cs
@@ -0,0 +1,10 @@
+﻿namespace Part2.OOP
+{
+    public class InterfaceCaller : IOne, ITwo
+    {
+        public void Aaaaa()
+        {
+            Console.WriteLine("What is going on..");
+        }
+    }
+}
