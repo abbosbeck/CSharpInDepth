93ddd04b075544f3076d3501045f8ee7ba1bba14
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-16 22:16:55 +0500

    Throw a text exception

---
+++ AspDotNetInDepth/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetInDepth.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("throw")]
+        public IActionResult ThrowError()
+        {
+            throw new Exception("This is a test exception!");
+        }
+    }
+}
