0ecb94469ddba720eb5013a0a9f8e37a6aa6eb9d
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-16 13:36:49 +0500

    How http Works

--- AspDotNetInDepth/Program.cs
+++ AspDotNetInDepth/Program.cs
@@ -19,7 +19,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+/*
+app.MapControllers();*/
 
-app.MapControllers();
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
