2b675a7c16b1a747487d119fb46f4812b054ae85
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-17 16:52:08 +0500

    Implement Version-Based Controller

---
+++ AspDotNetInDepth/Controllers/ApiVersioningController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetInDepth.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/products")]
+    public class ProductsV1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll() => Ok("Products from version 1");
+    }
+
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/products")]
+    public class ProductsV2Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll() => Ok("Products from version 2");
+    }
+}
