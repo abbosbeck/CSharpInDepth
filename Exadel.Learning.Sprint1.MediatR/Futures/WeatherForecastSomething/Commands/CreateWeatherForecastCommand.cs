60248d3e79abf6ff42c24514e0d50d68477bb3ae
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-03-26 02:10:28 +0500

    Implement Create Weather Forecast Logic

---
+++ Exadel.Learning.Sprint1.MediatR/Futures/WeatherForecastSomething/Commands/CreateWeatherForecastCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Microsoft.VisualBasic;
+
+namespace Exadel.Learning.Sprint1.MediatR.Futures.WeatherForecastSomething.Commands
+{
+    public record CreateWeatherForecastCommand(DateOnly Date, int TemperatureC, string summary) : IRequest<WeatherForecast>;
+}
--- Exadel.Learning.Sprint1.MediatR/WeatherForecast.cs
+++ Exadel.Learning.Sprint1.MediatR/WeatherForecast.cs
@@ -2,6 +2,7 @@ namespace Exadel.Learning.Sprint1.MediatR
 {
     public class WeatherForecast
     {
+        public int Id { get; set; }
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
