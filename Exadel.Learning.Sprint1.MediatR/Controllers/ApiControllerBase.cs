6d1c85f5d5ef731155749d2c3095598620238e88
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-03-27 23:46:16 +0500

    Implement Api Base Controller

---
+++ Exadel.Learning.Sprint1.MediatR/Controllers/ApiControllerBase.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exadel.Learning.Sprint1.MediatR.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiControllerBase : ControllerBase
+    {
+        protected ISender Mediator;
+        public ApiControllerBase(ISender mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+}
