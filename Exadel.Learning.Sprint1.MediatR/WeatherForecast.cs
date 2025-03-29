d15e1b7234be3e95f54b2d8ab0ed67993d19900a
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-03-29 23:23:10 +0500

    Code RUb

--- Exadel.Learning.Sprint1.MediatR/WeatherForecast.cs
+++ Exadel.Learning.Sprint1.MediatR/WeatherForecast.cs
@@ -7,7 +7,7 @@ namespace Exadel.Learning.Sprint1.MediatR
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)  (TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
