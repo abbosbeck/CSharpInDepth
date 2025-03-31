1b02c5257187861bff50700a840f54095764b6ba
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-04-01 01:12:06 +0500

    Add Migration Extension

---
+++ CSharpInDepth/CSharpInDepth.UserIdentity/Extensions/MigratoinExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CSharpInDepth.UserIdentity.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpInDepth.UserIdentity.Extensions
+{
+    public static class MigratoinExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+            using ApplicationDbContext context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            context.Database.Migrate();
+        }
+    }
+}
