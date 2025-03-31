d460e8970d79b7faa3980083704fc7d26cc03ab9
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-04-01 01:28:15 +0500

    Update AppDbContext

--- CSharpInDepth/CSharpInDepth.UserIdentity/Database/ApplicationDbContext.cs
+++ CSharpInDepth/CSharpInDepth.UserIdentity/Database/ApplicationDbContext.cs
@@ -4,7 +4,13 @@ namespace CSharpInDepth.UserIdentity.Database
 {
     public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
 
-     
+            builder.Entity<User>().Property(u => u.Intials).HasMaxLength(5);
+
+            builder.HasDefaultSchema("identityTablesss");
+        }
     }
 }
