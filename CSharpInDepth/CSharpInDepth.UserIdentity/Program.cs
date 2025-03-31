993c7af0d110ae9456384435394459c075d0ed93
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-04-01 01:08:23 +0500

    Set Up Authenication in Program.cs

--- CSharpInDepth/CSharpInDepth.UserIdentity/Program.cs
+++ CSharpInDepth/CSharpInDepth.UserIdentity/Program.cs
@@ -1,9 +1,20 @@
+using CSharpInDepth.UserIdentity.Database;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddCookie(IdentityConstants.ApplicationScheme);
+
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints();
 
 var app = builder.Build();
 
