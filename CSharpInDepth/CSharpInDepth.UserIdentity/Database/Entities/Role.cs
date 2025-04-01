df714e3d9d2b6556c091472c91d529fd50a945db
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-01 21:13:50 +0500

    Add Migration

--- CSharpInDepth/CSharpInDepth.UserIdentity/Database/Entities/Role.cs
+++ CSharpInDepth/CSharpInDepth.UserIdentity/Database/Entities/Role.cs
@@ -4,7 +4,7 @@ namespace CSharpInDepth.UserIdentity.Database.Entities
 {
     public class Role : IdentityRole, ISoftDeletable
     {
-        public bool IsDeleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public DateTime DeletedOn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsDeleted { get; set; }
+        public DateTime DeletedOn { get; set; }
     }
 }
