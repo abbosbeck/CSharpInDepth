49a5c612351d067a83ba3cd5d29ca8440080efa2
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-03 20:00:50 +0500

    Add Token Provider

---
+++ CSharpInDepth/Application/Authentication/TokenProvider.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace Application.Authentication
+{
+    class TokenProvider(IConfiguration configuration)
+    {
+        public string Create(User user)
+        {
+            string secretKey = configuration["Jwt:Secret"];
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+              Subject = new ClaimsIdentity
+              ([
+                  new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                  new Claim(JwtRegisteredClaimNames.PhoneNumber, user.PhoneNumber)
+              ]),
+                Expires = DateTime.UtcNow.AddMinutes(configuration.GetValue<int>("Jwt:ExpirationInMinutes")),
+                SigningCredentials = credentials,
+                Issuer = configuration["Jwt:Issuer"],
+                Audience = configuration["Jwt:Audience"]
+            };
+
+            var tokenHandler = new JsonWebTokenHandler();
+
+            string token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return token;
+        }
+    }
+}
