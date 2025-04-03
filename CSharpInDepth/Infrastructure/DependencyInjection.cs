906c4d1bf85cc5775ecf4f0ed15e89fbdee0d5ef
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-03 21:53:58 +0500

    Add Unit of Work

--- CSharpInDepth/Infrastructure/DependencyInjection.cs
+++ CSharpInDepth/Infrastructure/DependencyInjection.cs
@@ -19,6 +19,8 @@ namespace Infrastructure
 
             services.AddScoped<AppDbContextInitializer>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             return services;
         }
