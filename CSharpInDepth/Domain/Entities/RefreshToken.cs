18d91c1d897eaaa9080e28638ee6736069da9545
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-03 21:54:27 +0500

    Add Refresh Token Entity

---
+++ CSharpInDepth/Domain/Entities/RefreshToken.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; }
+        public string Token { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime ExpiresOnUtc { get; set; }
+
+        public User User { get; set; }
+    }
+}
