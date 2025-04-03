895f9025845269b7459fbe2c816d51b11f335ff0
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-03 17:26:49 +0500

    Add Register Command

---
+++ CSharpInDepth/Application/Members/Register/RegisterCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Members.Register
+{
+    public sealed record RegisterCommand(
+        string FirstName, 
+        string LastName, 
+        string PhoneNumber, 
+        string Password) 
+        : IRequest<UserResponse>;
+}
